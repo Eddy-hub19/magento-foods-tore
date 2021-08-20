4ee93fbce878a47c3645a4f794d0910e5940dcfb
Eddy-hub19 <sekaneduard@gmail.com>
2021-08-20 21:13:31 +0300

    add responsive and changes configures slider

--- app/design/frontend/Elogic/Majob/Magento_Theme/web/js/slider.js
+++ app/design/frontend/Elogic/Majob/Magento_Theme/web/js/slider.js
@@ -31,7 +31,7 @@ define([
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 1,
+                            slidesToShow: 3,
                             slidesToScroll: 1,
                         }
                     },
@@ -40,15 +40,15 @@ define([
                         settings: {
                             slidesToShow: 2,
                             slidesToScroll: 1,
-                            arrows: true
+                            arrows: false
                         }
                     },
                     {
-                        breakpoint: 480,
+                        breakpoint: 425,
                         settings: {
                             slidesToShow: 1,
                             slidesToScroll: 1,
-                            arrows: true
+                            arrows: false
                         }
                     },
                     {
@@ -56,7 +56,7 @@ define([
                         settings: {
                             slidesToShow: 1,
                             slidesToScroll: 1,
-                            arrows: true
+                            arrows: false
                         }
                     }
                 ]
